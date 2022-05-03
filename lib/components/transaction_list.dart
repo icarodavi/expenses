fcf8c9d4b850562bba6122bc5aebdc444f48667c
Icaro Davi Tavares Monteiro <icarodavi@gmail.com>
2022-05-03 12:19:30 -0300

    TextTheme and ternary operator

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,55 +10,72 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 500,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final tr = transactions[index];
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                const SizedBox(height: 20),
+                Text(
+                  'Sem transação cadastrada',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  height: 300,
+                  margin: const EdgeInsets.all(10),
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final tr = transactions[index];
 
-          return Card(
-            elevation: 2,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 15,
-                      vertical: 10,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      'R\$ ${tr.value.toStringAsFixed(2)}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    children: [
-                      Text(
-                        tr.title,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
+                return Card(
+                  elevation: 2,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.purple,
+                              width: 2,
+                            ),
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            'R\$ ${tr.value.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ),
+                          ),
                         ),
-                      ),
-                      Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: const TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  ),
-                ]),
-          );
-        },
-      ),
+                        Column(
+                          children: [
+                            Text(
+                              tr.title,
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                            Text(
+                              DateFormat('d MMM y').format(tr.date),
+                              style: const TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ]),
+                );
+              },
+            ),
     );
   }
 }
